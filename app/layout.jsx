518b0f45e393b796252d408e76124ce19a8e5a47
Oktavian Yudistira Putra <oktavianyudistiraputra@gmail.com>
2025-02-13 00:27:22 +0700

    cara membuat halaman web

--- app/layout.jsx
+++ app/layout.jsx
@@ -4,7 +4,13 @@ export default function Layout({ children }) {
             <head>
                 <title>Learn Next.js Fundamental</title>
             </head>
-            <body>{children}</body>
+            <body>
+                <header>[header]</header>
+                <main>
+                    {children}
+                </main>
+                <footer>[footer]</footer>
+            </body>
         </html>
     )
  };
---
+++ app/page.jsx
@@ -0,0 +1,8 @@
+export default function Home() {
+    return (
+        <>
+            <h1>Framework Next.js</h1>
+            <p>Hello, World!</p>
+        </>
+    );
+}
