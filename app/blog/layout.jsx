ea53a29e9f66896f7f64eb448c31e6293e36e593
Oktavian Yudistira Putra <oktavianyudistiraputra@gmail.com>
2025-02-20 09:23:49 +0700

    Menjalankan Project Next.Js Mode Production

--- app/blog/layout.jsx
+++ app/blog/layout.jsx
@@ -6,6 +6,7 @@ export default function BlogLayout({ children })
     useEffect(() => {
         window.alert("Hello client side!");
     }, []);
+    console.log('Production Mode: gunakan npm run build & npm start. agar semua modul sudah dirender dalam file .next, sehingga lebih ringan dalam mode prodcution! kalau npm run dev, semua modul dirender terus!');
     return (
         <div style={{ display: 'flex' }}>
             <div style={{ border: '1px solid black' }}>
