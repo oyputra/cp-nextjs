f17e3df9751752a528fc263dfd7cca9754b63100
oyputra <oktavianyudistiraputra@gmail.com>
2025-02-22 12:38:08 +0700

    Membuat Card Post List

--- app/blog/page.jsx
+++ app/blog/page.jsx
@@ -1,4 +1,6 @@
+import Card from "@/components/Card";
 import Heading from "@/components/Heading";
+import Link from "next/link";
 
 export const metadata = {
     title: "Blog - Belajar Next.js Fundamental"
@@ -8,8 +10,28 @@ export default function Blog() {
     return (
         <>
             <Heading>Blog</Heading>
-            <p>Halaman Blog</p>
-            <img src="/images/landing3.jpg" alt="" width={640} height={360} className="my-3 rounded-xl" />
+            <p className="text-xl mb-3">List of Post</p>
+            <div className="grid xl:grid-cols-4 gap-4">
+                <Card
+                    srcImage={"/images/landing5.jpg"}
+                    title={"Belajar Next.js"}
+                    description={"Membuat e-commerce from zero to hero"}
+                    link={"/blog/belajar-nextjs"}
+                />
+                <Card
+                    srcImage={"/images/landing6.jpg"}
+                    title={"Judul Berita"}
+                    description={"Berita terbaru dari judul-judul berita yang relevan"}
+                    link={"/blog/judul-berita"}
+                />
+                <Card
+                    srcImage={"/images/landing7.jpg"}
+                    title={"Latihan Route Next"}
+                    description={"Membuat route pada next js dengan perbedaan-perbedaan metodenya"}
+                    link={"/blog/latihan-route-next"}
+                />
+            </div>
+        
         </>
     );
 }
---
+++ components/Card.jsx
@@ -0,0 +1,18 @@
+import Link from "next/link";
+
+export default function Card({ srcImage, title, description, link }) {
+    return (
+        <div className="max-w-lg mx-auto">
+            <div className="bg-white shadow-md border border-gray-200 rounded-lg max-w-sm mb-5">
+                <img className="rounded-t-lg" src={srcImage} alt="" />
+                <div className="p-5">
+                    <h5 className="text-gray-900 font-bold text-2xl tracking-tight mb-2">{title}</h5>
+                    <p className="font-normal text-gray-700 mb-3">{description}</p>
+                    <Link href={link} className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-3 py-2 text-center inline-flex items-center">
+                        Read more
+                    </Link>
+                </div>
+            </div>
+        </div>
+    )
+}
