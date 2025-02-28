b5dc072ade16d73e56466a8455643042bf51adce
oyputra <oktavianyudistiraputra@gmail.com>
2025-03-01 01:09:55 +0700

    Cara Membaca File Markdown Di Next.Js

--- app/blog/belajar-nextjs/page.jsx
+++ app/blog/belajar-nextjs/page.jsx
@@ -1,14 +1,24 @@
 import Heading from "@/components/Heading";
+import { readFile } from "fs/promises";
+import path from "path";
 
 export const metadata = {
     title: "Belajar Next.js - Belajar Next.js Fundamental"
 }
 
-export default function BelajarNextJs() {
+export default async function BelajarNextJs() {
+    const filePath = path.resolve(process.cwd(), "content/blog/belajar-nextjs.md");
+    let text = "";
+    try {
+        text = await readFile(filePath, "utf8");
+    } catch (error) {
+        console.error("Error reading file:", error);
+        text = "Gagal memuat konten.";
+    }
     return (
         <>
             <Heading>Belajar Next.js</Heading>
-            <p>Halaman Belajar Next.js</p>
+            <p>{text}</p>
             <img src="/images/landing5.jpg" alt="" width={640} height={360} className="my-3 rounded-xl" />
         </>
     );
---
+++ content/blog/belajar-nextjs.md
@@ -0,0 +1,12 @@
+# Heading
+
+## Sub-Heading
+
+Rich text with **bold** and _italic_.
+
+new paragraph
+
+List :
+* one 
+* two 
+* three
