2651d870da7fa5e9928cc5afb629fc65dbc4a1ca
oyputra <oktavianyudistiraputra@gmail.com>
2025-03-01 10:50:30 +0700

    Memisahkan Layer Data Dengan Layer Ui

--- app/blog/belajar-nextjs/page.jsx
+++ app/blog/belajar-nextjs/page.jsx
@@ -1,23 +1,19 @@
 import Heading from "@/components/Heading";
-import { readFile } from "fs/promises";
-import matter from "gray-matter";
-import { marked } from "marked";
+import { getPost } from "@/lib/post";
 
 export const metadata = {
     title: "Belajar Next.js - Belajar Next.js Fundamental"
 }
 
 export default async function BelajarNextJs() {
-    const text = await readFile("content/blog/belajar-nextjs.md", "utf8");
-    const { data: { title, image, date, author }, content } = matter(text)    
-    console.log(title, image, date, author, content);
-    const html = marked(content);
+    const post = await getPost("belajar-nextjs");
+
     return (
         <>
-            <Heading>{title}</Heading>
-            <p className="text-sm mb-5 text-gray-500">{author} - {date}</p>
-            <img src={image} alt="" width={640} height={360} className="mb-5 rounded-xl" />
-            <article dangerouslySetInnerHTML={{ __html: html }} className="prose max-w-screen-sm prose-slate" />
+            <Heading>{post.title}</Heading>
+            <p className="text-sm mb-5 text-gray-500">{post.author} - {post.date}</p>
+            <img src={post.image} alt="" width={640} height={360} className="mb-5 rounded-xl" />
+            <article dangerouslySetInnerHTML={{ __html: post.body }} className="prose max-w-screen-sm prose-slate" />
         </>
     );
 }
