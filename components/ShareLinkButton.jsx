9aca07e7fb3356b55b6512b94e08e3853340857f
Oktavian Yudistira Putra <oktavianyudistiraputra@gmail.com>
2025-03-09 10:10:26 +0700

    Membuat Fungsi Copy Link Dengan Client Component

--- components/ShareLinkButton.jsx
+++ components/ShareLinkButton.jsx
@@ -1,14 +1,18 @@
 'use client';
+import { useState } from "react";
 
 export default function ShareLinkButton() {
+    const [copied, setCopied] = useState(false);
     function handleClick() {
-        console.log('clicked!');
+        navigator.clipboard.writeText(window.location.href);
+        setCopied(true);
+        setTimeout(() => setCopied(false), 1500);
     }
     return (
         <button 
         onClick={handleClick}
         className="border px-2 py-1 rounded text-gray-500 text-sm hover:bg-gray-200 hover:text-gray-700">
-            Copy Link
+            { copied ? "Link Copied" : "Share Link" }
         </button>
     );
 }
