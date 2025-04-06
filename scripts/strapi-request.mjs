ff89699045d356fd80a17e857df1f0ab642665e8
Oktavian Yudistira Putra <oktavianyudistiraputra@gmail.com>
2025-04-06 20:05:35 +0700

    Menentukan Data Response Dari Strapi Dengan Parameter

--- scripts/strapi-request.mjs
+++ scripts/strapi-request.mjs
@@ -1,5 +1,20 @@
 import { writeFileSync } from 'node:fs';
-const url = "http://localhost:1337/api/posts?populate=*";
+import qs from 'qs';
+const url = "http://localhost:1337/api/posts?" + qs.stringify(
+    { 
+        fields: ["slug", "title", "description", "publishedAt", "author", "body"],
+        populate: { 
+            image: {
+            fields: ["url"]
+            } 
+        },
+        sort: ["publishedAt:desc"],
+        pagination: {
+            pageSize: 3
+        }
+    },
+    { encodeValuesOnly: true },
+);
 const response = await fetch(url);
 const body = await response.json();
 const posts = JSON.stringify(body, null, 2);
